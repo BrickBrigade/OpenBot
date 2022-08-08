fb461fd53d4e4d05902228842322c94bca6d51e2
BrickBrigade <101427661+BrickBrigade@users.noreply.github.com>
2022-08-08 02:06:47 -0500

    init

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,20 +1,39 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
 
 module.exports = (client) => {
 	client.handleCommands = async () => {
 		const commandFolders = fs.readdirSync('./src/commands');
 		for (const folder of commandFolders) {
+			console.log(folder)
 			const commandFiles = fs
 				.readdirSync(`./src/commands/${folder}`)
 				.filter((file) => file.endsWith('.js'));
 
 			const { commands, commandArray } = client;
 			for (const file of commandFiles) {
-				const commands = require(`../../commands/${folder}/${file}`)
+				const command = require(`../../commands/${folder}/${file}`);
 				commands.set(command.data.name, command);
 				commandArray.push(command.data.toJSON());
-				console.log(`Command: ${command.data.name} has passed through the handler`);
+				console.log(
+					`Command: ${command.data.name} has passed through the handler`
+				);
 			}
+		}
+
+		const clientId = '1005145943282286753';
+		const rest = new REST({ version: '9' }).setToken(process.env.token);
+		try {
+			console.log('Started refreshing application (/) commands.');
+
+			await rest.put(Routes.applicationCommands(clientId), {
+				body: client.commandArray,
+			});
+
+			console.log('Successfully reloaded application (/) commands.');
+		} catch (error) {
+			console.error(error);
 		}
 	};
 };
