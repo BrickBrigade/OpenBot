9f6b4dba069dd5bee2929bdad17438f445f8b157
BrickBrigade <101427661+BrickBrigade@users.noreply.github.com>
2022-08-14 17:05:26 -0500

    finished "Get Voice Members" button

--- src/components/buttons/getVoiceMembers.js
+++ src/components/buttons/getVoiceMembers.js
@@ -1,14 +1,25 @@
-const { VoiceChannel } = require("discord.js");
-
 module.exports = {
 	data: {
 		name: `getVoiceMembers`,
 	},
 	async execute(interaction, client) {
 
-		members = VoiceChannel.members
-		await interaction.reply({
-			content: `voiceChannel: unknown`,
-		});
-	},
+		
+
+		const guild = client.guilds.cache.get(interaction.guildId)
+		const member = guild.members.cache.get(interaction.member.user.id)
+		const voiceChannel = member.voice.channel
+		const vcMemberListSize = voiceChannel.members.size
+		const memberArray = []
+		voiceChannel.members.forEach(user => {memberArray.push(user.displayName)})
+
+		const message = `There is only ${vcMemberListSize} person in ${voiceChannel}. You are all alone, ${member.displayName}`
+		const message2 = `There are ${vcMemberListSize} people in ${voiceChannel} including ${memberArray.toString()}`
+
+		if (vcMemberListSize === 1) {
+			await interaction.reply(message)
+		} else {
+			await interaction.reply(message2)
+		}
+	}
 };
